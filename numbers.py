2eedb6e2a49d92c7a9af203eccb9177849ab61cd
GuDev154 <gustavoantonholi3@gmail.com>
2025-03-23 10:08:14 -0300

    Update numbers.py

--- numbers.py
+++ numbers.py
@@ -5,3 +5,12 @@ b = random.randint(30, 60)
 soma = a + b
 subtracao = a - b
 multiplicacao = a * b
+divisao = a / b
+
+def menu():
+    print("-- Menu de operações --")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão")
+    
