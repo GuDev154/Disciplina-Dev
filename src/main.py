56c2359ca51f9610b9dccab44be8086fe3a88b7f
GuDev154 <gustavoantonholi3@gmail.com>
2025-04-24 18:45:27 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 import random
-from pydantic import BaseModel
+import pydantic
 
 app = FastAPI()
 
-class Estudante(BaseModal):
+class Estudante(pydantic.BaseModal):
     nome: str
     curso: str
     ativo: bool
