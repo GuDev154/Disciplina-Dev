2c0d59dc1274b7cc909638244e901490b94fe41b
GuDev154 <gustavoantonholi3@gmail.com>
2025-04-24 16:51:06 -0300

    update Pr

--- test/test.py
+++ test/test.py
@@ -13,18 +13,17 @@ def test_funcaoteste():
     assert result == {"teste": True, "num_aleatorio": 12345}
 
 
-def test_create_estudante(estudante: Estudante):
+def test_create_estudante():
     estudante_teste = Estudante(name="Fulano", curso ="Curso 1", ativo=False)
-    assert estudante_teste == create_estudante()
+    assert estudante_teste == create_estudante(estudante_teste)
 
-def test_update_estudante_negativo(id_estudante: int):
+def test_update_estudante_negativo():
     assert not update_estudante(-5)
 
-def test_update_estudante_positivo(id_estudante: int):
+def test_update_estudante_positivo():
     assert update_estudante(10)
 
-
-def test_delete_estudante_negativo(id_estudante: int):
+def test_delete_estudante_negativo():
     assert not update_estudante(-5)
 
 def test_delete_estudante_positivo(id_estudante: int):
